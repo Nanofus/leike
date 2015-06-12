6031dfa641c28cb1c11deb8975e4ca34ced5b9af
Ville Talonpoika <talonvi@gmail.com>
2015-06-13 00:12:17 +0300

    Image saving

--- leike/public_html/app/controllers/main_controller.js
+++ leike/public_html/app/controllers/main_controller.js
@@ -1,30 +1,45 @@
 leike.controller('MainController', ['$scope', '$interval', function ($scope, $interval) {
         var remote = require('remote');
         var clipboard = remote.require('clipboard');
-        var currentClipboard;
+        var currentClipboard = clipboard.readText();
         var fs = remote.require('fs');
 
         $scope.items = new Array();
 
-        function saveClipboard() {
-            if (clipboard.readText() !== $scope.items[$scope.items.length - 1]) {
-                $scope.items.push(clipboard.readText());
-            } else if (clipboard.readImage() !== $scope.items[$scope.items.length - 1]) {
-                console.log("Image detected! fs: "+fs);
-                /*fs.writeFile('tempimage.png', clipboard.readImage().toPng(), function (err) {
-                    if (err) {
-                        return console.log(err);
-                    }
-                });*/
+        function saveToClipboard() {
+            var clipboardData;
 
+            if (clipboard.readText() !== currentClipboard && clipboard.readText() !== "") {
+                clipboardData = clipboard.readText();
+
+                console.log("Data is text! - " + clipboardData);
+
+                currentClipboard = clipboardData;
+                $scope.items.push(clipboardData);
+            } else if (clipboard.readImage() !== currentClipboard) {
+                writeImage(clipboard.readImage().toPng());
+                clipboardData = "[image]";
+
+                console.log("Data is an image! - " + clipboardData);
+
+                currentClipboard = clipboardData;
+                $scope.items.push(clipboardData);
             }
+        }
+
+        function writeImage(data) {
+            fs.writeFile('clipboard_images/tempimage.png', data, function (err) {
+                if (err)
+                    throw err;
+                console.log('It\'s saved!');
+            });
         }
 
         //Auto-refresh
         $interval(function () {
-            if (clipboard.readText() !== currentClipboard) {
-                saveClipboard();
-                currentClipboard = clipboard.readText();
+            if ((clipboard.readText() !== currentClipboard && clipboard.readText !== "") ||
+                    (clipboard.readImage() !== currentClipboard && !clipboard.readImage().isEmpty())) {
+                saveToClipboard();
             }
         }, 10, 0, false);
 
