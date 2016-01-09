78416da004d9f60eb9d51d536b14fd4e87d4fda4
Ville Talonpoika <talonvi@gmail.com>
2016-01-09 20:57:11 +0200

    Updated Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,20 +4,29 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
+var del = require('del');
 
 gulp.task('sass', function () {
   gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./build/css'));
 });
+
 gulp.task('coffee', function() {
   gulp.src('./src/*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
-    .pipe(gulp.dest('./app/'));
+    .pipe(gulp.dest('./build/'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('./scss/**/*.scss', ['sass']);
   gulp.watch('./src/*.coffee', ['coffee']);
 });
-gulp.task('default', ['watch']);
+
+gulp.task('clean', function() {
+    return del(['./build']);
+});
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('sass', 'coffee');
+});
