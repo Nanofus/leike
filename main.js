e1498f4b4a01c3c22ea7b03e05c2bff520761429
Ville Talonpoika <talonvi@gmail.com>
2016-01-09 22:45:21 +0200

    Added header

--- main.js
+++ main.js
@@ -6,8 +6,12 @@ const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
-const remote = require('electron').remote;
-const clipboard = require('electron').clipboard;
+const remote = electron.remote;
+const clipboard = electron.clipboard;
+const Menu = require('menu');
+const Tray = require('tray');
+
+var appIcon = null;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -15,11 +19,9 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 400, height: 800});
-
+  mainWindow = new BrowserWindow({width: 800, height: 900, frame: false, 'min-width': 300, 'min-height': 350});
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
@@ -36,15 +38,30 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 app.on('ready', function () {
     createWindow();
+
+    appIcon = new Tray('img/icon.png');
+    var contextMenu = Menu.buildFromTemplate([
+      {label: 'Show', click: function () {
+        mainWindow.show();
+      }},
+      {label: 'Quit', click: function () {
+        app.quit();
+      }}
+    ]);
+    appIcon.setToolTip('leike');
+    appIcon.setContextMenu(contextMenu);
+    appIcon.addListener('clicked', function () {
+      mainWindow.show();
+    });
 });
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  /* if (process.platform !== 'darwin') {
     app.quit();
-  }
+  }*/
 });
 
 app.on('activate', function () {
--- src/app.coffee
+++ src/app.coffee
@@ -10,6 +10,22 @@ entries = new Array
 
 filePath = app.getPath('desktop') + '\\leike_images\\'
 
+header = new Vue(
+  el: 'header'
+  methods:
+    closeWindow: ->
+      remote.getCurrentWindow().hide()
+
+    maximizeWindow: ->
+      if !remote.getCurrentWindow().isMaximized()
+        remote.getCurrentWindow().maximize()
+      else
+        remote.getCurrentWindow().unmaximize()
+
+    minimizeWindow: ->
+      remote.getCurrentWindow().minimize()
+)
+
 vm = new Vue(
   el: '#entry-list'
   data: entries: entries
